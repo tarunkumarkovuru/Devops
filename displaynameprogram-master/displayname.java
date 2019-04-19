76c098dffb5f20846ab32b12cb00599dd786c0c9
tarunkumarkovuru <tarunkumarfamily@gmail.com>
2019-04-19 11:56:04 +0530

    This is copied by tarun from anilsir github

---
+++ displaynameprogram-master/displayname.java
@@ -0,0 +1,11 @@
+
+public class displayname {
+	public static void main (String [] args){
+	   for (int i =1; i<=10;i++){
+	       System.out.println("CubeIPL Informatics:"+i);	   
+
+   	    }
+
+	}
+
+}
